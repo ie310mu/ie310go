1d538554f0ad88df4b7a4adcd31ae809e49608a1
ie310 <ie35725551@qq.com>
2019-08-19 15:27:49 +0800

    1
    1

--- route/JSONServiceResult.go
+++ route/JSONServiceResult.go
@@ -4,7 +4,7 @@ import "github.com/ie310mu/ie310go/common/json"
 
 //JSONServiceResult ..
 type JSONServiceResult struct {
-	State      int         `json:"state"` //不能加omitempty，否则为0时不输入，js中解析为undefined
+	State      int         `json:"state"` //不能加omitempty，否则为0时不输出，js中解析为undefined
 	Message    string      `json:"message"`
 	Data       interface{} `json:"data"`
 	DataJSON   string      `json:"dataJSON"`
